a10a14f6b7787d7d26acc43038f4db1b41ec0677
Jivago Alves <jivagoalves@gmail.com>
2011-10-02 16:53:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workdays::Application.config.secret_token = '0f5d9aa0c92eb58dbe6158ee975157a765c37c264e37f449f8e98244f00042d92e6b8fe9a8a5b094a6f6569b1ec5b0fdce36306532fdaa791d91ee9ac1c6e917'
