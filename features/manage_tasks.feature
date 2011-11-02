35b1b7b0e8b93253ed33dc7af0a8e5d5725671cb
Jivago Alves <jivagoalves@gmail.com>
2011-11-01 23:52:10 -0400

    Added scenarios to create, delete and update a task

--- features/manage_tasks.feature
+++ features/manage_tasks.feature
@@ -6,7 +6,26 @@ Feature: Manage Tasks
   Background:
     Given I am signed in as Mara
   
-  Scenario: Report Spent Time on a Task
+  Scenario: User creates a task
+    Given I have no task
+    When I go to the list of tasks
+    And I create a task described as "Have fun on saturday"
+    Then I should see a task described "Have fun on saturday"
+  
+  Scenario: User deletes a task
+    Given I have a task described as "Buy food"
+    When I go to the list of tasks
+    And I delete the task described as "Buy food"
+    Then I should not see the task described as "Buy food"
+  
+  Scenario: User updates an existing task
+    Given I have a task described as "Clean the house"
+    When I go to the list of tasks
+    And I update the task description from "Clean the house" to "Clean the room"
+    Then I should not see the task described as "Clean the house"
+    And I should see the task described as "Clean the room"
+  
+  Scenario: User reports spent time on a task
     Given I have a task
     When I go to the list of tasks
     And I report 10 minutes spent on the task
--- features/step_definitions/manage_tasks_steps.rb
+++ features/step_definitions/manage_tasks_steps.rb
@@ -21,3 +21,42 @@ end
 Then /^I should see "([^"]*)"$/ do |minutes|
   page.should have_content(minutes)
 end
+
+Given /^I have a task described as "([^"]*)"$/ do |description|
+  @task = Factory(:task, :user_id => @user.id, :description => description)
+end
+
+When /^I delete the task described as "([^"]*)"$/ do |description|
+  click_button 'X'
+end
+
+Then /^I should not see the task described as "([^"]*)"$/ do |description|
+  page.should have_no_content(description)
+end
+
+Given /^I have no task$/ do
+end
+
+When /^I create a task described as "([^"]*)"$/ do |description|
+  within("form.new_task") do
+    fill_in 'task_description', :with => description
+    click_button 'Add'
+  end
+end
+
+Then /^I should see a task described "([^"]*)"$/ do |description|
+  page.should have_content(description)
+end
+
+When /^I update the task description from "([^"]*)" to "([^"]*)"$/ do |old_description, new_description|
+  click_link old_description
+  within("form.edit_task") do
+    fill_in 'task_description', :with => new_description
+    click_button 'Save'
+  end
+end
+
+Then /^I should see the task described as "([^"]*)"$/ do |description|
+  visit tasks_path
+  page.should have_content(description)
+end
